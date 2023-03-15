c4725d775b5800d817a7f7782d442c45f6fb9667
hsu016 <jessiehsu27@gmail.com>
2023-03-15 15:49:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-import sys
-import json
-from datetime import datetime
+# import sys
+# import json
+# from datetime import datetime
 
-import requests
+# import requests
 from flask import Flask, request
 
 from facebookbot import (
@@ -55,121 +55,126 @@ def verify():
 
 
 @app.route('/callback', methods=['POST'])
-def webhook():
-
-    # endpoint for processing incoming messaging events
-
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
-    if data["object"] == "page":
-
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "roger that!")
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
-
-    return "ok", 200
-
-
-def send_message(recipient_id, message_text):
-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": ACCESS_TOKEN
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
-
-def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
-    try:
-        if type(msg) is dict:
-            msg = json.dumps(msg)
-        else:
-            msg = unicode(msg).format(*args, **kwargs)
-        print("{}: {}".format(datetime.now(), msg))
-    except UnicodeEncodeError:
-        pass  # squash logging errors in case of non-ascii text
-    sys.stdout.flush()
-
 # def webhook():
 
 #     # endpoint for processing incoming messaging events
 
-#     body = request.get_json()
-# #     log(data)
-#     handler.handle(body)
-    
-    
-# #     if data['object'] == 'page':
-# #       for entry in data['entry']:
-# #        for messaging_event in entry['messaging']:
-# #     # IDs
-# #         sender_id = messaging_event['sender']['id']
-# #         recipient_id = messaging_event['recipient']['id']
-# #     if messaging_event.get('message'):
-# #          # Extracting text message
-# #          if 'text' in messaging_event['message']:
-# #           messaging_text = messaging_event['message']['text']
-# #          else:
-# #           messaging_text = 'no text'
-# #     # Echo
-# #          response = messaging_text
-# #          bot.send_text_message(sender_id, messaging_text)
+#     data = request.get_json()
+#     log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+#     if data["object"] == "page":
+
+#         for entry in data["entry"]:
+#             for messaging_event in entry["messaging"]:
+
+#                 if messaging_event.get("message"):  # someone sent us a message
+
+#                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+#                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+#                     message_text = messaging_event["message"]["text"]  # the message's text
+
+#                     send_message(sender_id, "roger that!")
+
+#                 if messaging_event.get("delivery"):  # delivery confirmation
+#                     pass
+
+#                 if messaging_event.get("optin"):  # optin confirmation
+#                     pass
+
+#                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+#                     pass
+
 #     return "ok", 200
 
 
-# # def log(message):
-# #      print(message)
-# #      sys.stdout.flush()
+# def send_message(recipient_id, message_text):
+
+#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+#     params = {
+#         "access_token": ACCESS_TOKEN
+#     }
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+#     data = json.dumps({
+#         "recipient": {
+#             "id": recipient_id
+#         },
+#         "message": {
+#             "text": message_text
+#         }
+#     })
+#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+#     if r.status_code != 200:
+#         log(r.status_code)
+#         log(r.text)
 
 
-# @handler.add(GetStartedEvent)
-# def handle_get_started(event):
+# def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+#     try:
+#         if type(msg) is dict:
+#             msg = json.dumps(msg)
+#         else:
+#             msg = unicode(msg).format(*args, **kwargs)
+#         print("{}: {}".format(datetime.now(), msg))
+#     except UnicodeEncodeError:
+#         pass  # squash logging errors in case of non-ascii text
+#     sys.stdout.flush()
+
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    body = request.get_json()
+#     log(data)
+    handler.handle(body)
     
-#     fb_bot_api.push_message(
-#         user_id, 
-#         message=TextSendMessage(text='Welcome to this page/app...')
-#     )    
     
-#     # set welcome message   
+#     if data['object'] == 'page':
+#       for entry in data['entry']:
+#        for messaging_event in entry['messaging']:
+#     # IDs
+#         sender_id = messaging_event['sender']['id']
+#         recipient_id = messaging_event['recipient']['id']
+#     if messaging_event.get('message'):
+#          # Extracting text message
+#          if 'text' in messaging_event['message']:
+#           messaging_text = messaging_event['message']['text']
+#          else:
+#           messaging_text = 'no text'
+#     # Echo
+#          response = messaging_text
+#          bot.send_text_message(sender_id, messaging_text)
+    return "ok", 200
 
-# @handler.add(TextMessageEvent)
-# def handle_text_message(event):
+
+# def log(message):
+#      print(message)
+#      sys.stdout.flush()
+
+
+@handler.add(GetStartedEvent)
+def handle_get_started(event):
+    
+    fb_bot_api.push_message(
+        user_id, 
+        message=TextSendMessage(text='Welcome to this page/app...')
+    )    
+    
+    # set welcome message   
+
+@handler.add(TextMessageEvent)
+def handle_text_message(event):
         
-#     text = event.message.text
+    text = event.message.text
     
-#     user_id = event.sender.id
+    user_id = event.sender.id
+    
+    fb_bot_api.push_message(
+        user_id, 
+        message=text
+    )
     
 #     if text == "profile":
         
@@ -351,74 +356,74 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
         
 #         fb_bot_api.broadcast(message = text_message)
     
-# @handler.add(AttachmentMessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
-# def handle_content_message(event):
+@handler.add(AttachmentMessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
     
-#     url = event.message.attachment.payload.url
+    url = event.message.attachment.payload.url
     
-#     user_id = event.sender.id
+    user_id = event.sender.id
     
-#     if isinstance(event.message.attachment, ImageMessage):
-#         pass
-#         fb_bot_api.push_message(
-#             user_id, 
-#             message=ImageSendMessage(
-#                 url = url
-#             )
-#         )
+    if isinstance(event.message.attachment, ImageMessage):
+        pass
+        fb_bot_api.push_message(
+            user_id, 
+            message=ImageSendMessage(
+                url = url
+            )
+        )
         
-#     elif isinstance(event.message.attachment, VideoMessage):
+    elif isinstance(event.message.attachment, VideoMessage):
         
-#         fb_bot_apipush_message(
-#             user_id, 
-#             message=VideoSendMessage(
-#                 url = url
-#             )
-#         )        
+        fb_bot_apipush_message(
+            user_id, 
+            message=VideoSendMessage(
+                url = url
+            )
+        )        
         
-#     elif isinstance(event.message.attachment, AudioMessage):
+    elif isinstance(event.message.attachment, AudioMessage):
         
-#         fb_bot_apipush_message(
-#             user_id, 
-#             message=AudioSendMessage(
-#                 url = url
-#             )
-#         )        
+        fb_bot_apipush_message(
+            user_id, 
+            message=AudioSendMessage(
+                url = url
+            )
+        )        
         
-#     else:
-#         return    
+    else:
+        return    
     
     
-# @handler.add(AttachmentMessageEvent, message=FileMessage)
-# def handle_file_message(event):
+@handler.add(AttachmentMessageEvent, message=FileMessage)
+def handle_file_message(event):
     
-#     print(event.message.attachment.type)
+    print(event.message.attachment.type)
     
-# @handler.add(AttachmentMessageEvent, message=LocationMessage)
-# def handle_location_message(event):    
+@handler.add(AttachmentMessageEvent, message=LocationMessage)
+def handle_location_message(event):    
 
-#     print(event.message.attachment.type)
+    print(event.message.attachment.type)
     
-# @handler.add(AttachmentMessageEvent, message=FallbackMessage)
-# def handle_fallback_message(event):    
+@handler.add(AttachmentMessageEvent, message=FallbackMessage)
+def handle_fallback_message(event):    
 
-#     print(event.message.attachment.type)
+    print(event.message.attachment.type)
 
 
-# @handler.add(PostbackEvent)
-# def handle_postback_message(event):
+@handler.add(PostbackEvent)
+def handle_postback_message(event):
 
-#     postback_payload = event.postback.payload
+    postback_payload = event.postback.payload
     
-#     print(postback_payload)
+    print(postback_payload)
 
-# @handler.add(LinkingEvent)
-# def handle_linking(event):
-#     print("event.LinkingEvent")
+@handler.add(LinkingEvent)
+def handle_linking(event):
+    print("event.LinkingEvent")
     
-# @handler.add(UnLinkingEvent)
-# def handle_unlinking(event):
-#     print("event.UnLinkingEvent")    
+@handler.add(UnLinkingEvent)
+def handle_unlinking(event):
+    print("event.UnLinkingEvent")    
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
