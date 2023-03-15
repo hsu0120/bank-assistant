2a7c638a6231b76e4e748b9999428396dce1f937
hsu016 <jessiehsu27@gmail.com>
2023-03-15 17:02:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import os
-# import sys
-# import json
-# from datetime import datetime
-
-# import requests
 from flask import Flask, request
 
 from facebookbot import (
@@ -55,103 +50,14 @@ def verify():
 
 
 @app.route('/callback', methods=['POST'])
-# def webhook():
-
-#     # endpoint for processing incoming messaging events
-
-#     data = request.get_json()
-#     log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
-#     if data["object"] == "page":
-
-#         for entry in data["entry"]:
-#             for messaging_event in entry["messaging"]:
-
-#                 if messaging_event.get("message"):  # someone sent us a message
-
-#                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-#                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-#                     message_text = messaging_event["message"]["text"]  # the message's text
-
-#                     send_message(sender_id, "roger that!")
-
-#                 if messaging_event.get("delivery"):  # delivery confirmation
-#                     pass
-
-#                 if messaging_event.get("optin"):  # optin confirmation
-#                     pass
-
-#                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-#                     pass
-
-#     return "ok", 200
-
-
-# def send_message(recipient_id, message_text):
-
-#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-#     params = {
-#         "access_token": ACCESS_TOKEN
-#     }
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
-#     data = json.dumps({
-#         "recipient": {
-#             "id": recipient_id
-#         },
-#         "message": {
-#             "text": message_text
-#         }
-#     })
-#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-#     if r.status_code != 200:
-#         log(r.status_code)
-#         log(r.text)
-
-
-# def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
-#     try:
-#         if type(msg) is dict:
-#             msg = json.dumps(msg)
-#         else:
-#             msg = unicode(msg).format(*args, **kwargs)
-#         print("{}: {}".format(datetime.now(), msg))
-#     except UnicodeEncodeError:
-#         pass  # squash logging errors in case of non-ascii text
-#     sys.stdout.flush()
-
 def webhook():
 
     # endpoint for processing incoming messaging events
 
     body = request.get_json()
-#     log(data)
     handler.handle(body)
     
-    
-#     if data['object'] == 'page':
-#       for entry in data['entry']:
-#        for messaging_event in entry['messaging']:
-#     # IDs
-#         sender_id = messaging_event['sender']['id']
-#         recipient_id = messaging_event['recipient']['id']
-#     if messaging_event.get('message'):
-#          # Extracting text message
-#          if 'text' in messaging_event['message']:
-#           messaging_text = messaging_event['message']['text']
-#          else:
-#           messaging_text = 'no text'
-#     # Echo
-#          response = messaging_text
-#          bot.send_text_message(sender_id, messaging_text)
     return "ok", 200
-
-
-# def log(message):
-#      print(message)
-#      sys.stdout.flush()
 
 
 @handler.add(GetStartedEvent)
@@ -170,21 +76,7 @@ def handle_text_message(event):
     text = event.message.text
     
     user_id = event.sender.id
-    
-#     fb_bot_api.push_message(
-#         user_id, 
-#         message=TextSendMessage(text=text)
-#     )
-    
-    
-#     if text == "profile":
-        
-#         profile = fb_bot_api.get_profile(user_id)
-        
-#         fb_bot_api.push_message(
-#             user_id, 
-#             message=TextSendMessage(text='Display name: ' + profile.last_name + profile.first)
-#         )
+
 #     if text == "buttons":
         
 #         buttons_template_message = TemplateSendMessage(
@@ -321,28 +213,28 @@ def handle_text_message(event):
             message=TextSendMessage(
                 text = "Quick reply",
                 quick_replies = [
-                    TextQuickReply(title="title1", payload="payload_1"),
-                    TextQuickReply(title="title2", payload="payload_2")
+                    TextQuickReply(title="q1", payload="q1"),
+                    TextQuickReply(title="q2", payload="q2")
                 ]          
             )
         )
         
         # Attachment Message quick reply
         
-        buttons_template = ButtonsTemplate(
-            text="buttons_template",
-            buttons= [
-                PostbackAction(title="action_1", payload="payload_1"),
-                PostbackAction(title="action_2", payload="payload_2")
-            ])
+        # buttons_template = ButtonsTemplate(
+        #     text="buttons_template",
+        #     buttons= [
+        #         PostbackAction(title="action_1", payload="payload_1"),
+        #         PostbackAction(title="action_2", payload="payload_2")
+        #     ])
 
-        templateSendMessage = TemplateSendMessage(
-            template=buttons_template,
-            quick_replies = [
-                TextQuickReply(title="title1", payload="payload_1"),
-                TextQuickReply(title="title2", payload="payload_2")
-            ]
-        )       
+        # templateSendMessage = TemplateSendMessage(
+        #     template=buttons_template,
+        #     quick_replies = [
+        #         TextQuickReply(title="title1", payload="payload_1"),
+        #         TextQuickReply(title="title2", payload="payload_2")
+        #     ]
+        # )       
 
 
         fb_bot_api.push_message(
