764340be84d355221500bbf541a84ffa8976fc21
hsu016 <jessiehsu27@gmail.com>
2023-03-16 14:50:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ def handle_text_message(event):
             message=TextSendMessage(
                 text = "Quick reply",
                 quick_replies = [
-                    PostbackAction(title="test_1", payload="reply_1")
+                    PostbackAction(title="test_1", payload="reply_1"),
                     TextQuickReply(title="q1", payload="q1"),
                     TextQuickReply(title="q2", payload="q2")
                 ]          
