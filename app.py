dda5a70d99c8b6c6366c1106a19eeac795bd2ede
hsu016 <jessiehsu27@gmail.com>
2023-03-16 23:16:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ handler = WebhookHandler()
 
 openai.api_key = CHATGPT_TOKEN
 
-file_id = openai.File.create(file=open("example.jsonl"), purpose="classifications")
+file_id = openai.File.create(file=open("example.jsonl"), purpose="classification")
 print(file_id)
 
 data = dict()
