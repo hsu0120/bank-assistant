70a4f8a0771e73776f4d885795cf23f04951deba
hsu016 <jessiehsu27@gmail.com>
2023-03-22 16:34:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -371,5 +371,5 @@ def handle_postback_message(event):
         )
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run('0.0.0.0',port=port)
